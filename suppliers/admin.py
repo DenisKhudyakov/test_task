00fc245d103733c88dfcf54840c7e97cbce7074d
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-08-10 19:56:59 +0500

    update model, create serrializator, crate admin panel

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from suppliers.models import NetworkNode, Products
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    """Админ панель сети"""
+    list_display: tuple[str] = (
+        "id",
+        "name",
+        "email",
+        "country",
+        "city",
+        "street",
+        "house_number",
+        "supplier",
+        "debt",
+        "creation_time",
+        "hierarchy_level",
+        "products",
+    )
+    list_filter: tuple[str] = ("city",)
+    search_fields: tuple[str] = ("name", "city",)
+    actions = ["clear_debt",]
+
+    def supplier_link(self, obj):
+        """Ссылка на товар"""
+        if obj.supplier:
+            return f'<a href="/admin/suppliers/networknode/{obj.supplier.id}/change/">{obj.supplier.name}</a>'
+        return "Нет данных поставщика"
+
+    supplier_link.allow_tags = True
+    supplier_link.short_description = 'Supplier'
+
+    def clear_debt(self, request, queryset):
+        """Очистка долга"""
+        queryset.update(debt=0.00)
+        self.message_user(request, "Долг очищен")
+
+    clear_debt.short_description = "Очистить долг"
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    """Админ панель товаров"""
+    list_display: tuple[str] = ("id", "name", "model", "data")
+    search_fields: tuple[str] = ("name", "model",)
--- suppliers/models.py
+++ suppliers/models.py
@@ -1,32 +1,49 @@
 """
-Здесь находятся модели приложения поставщиков и их связи
-Модели: Завод, Индивидуальный Предприниматель, Розничная Сеть, Контакты, Продукты
+Здесь находятся модели приложения поставщиков
+Модель NetworkNode может быть одной из следующих: Завод, Индивидуальный Предприниматель, Розничная Сеть
+Модель Product это продукт, которые реализует поставщик
 """
 
-import datetime
+from django.utils import timezone
 
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from config.settings import NULLABLE
 
 
-class Contacts(models.Model):
-    """Модель контактов поставщика"""
+class NetworkNode(models.Model):
+    """Модель для хранения данных сетевого узла"""
+
+    name: str = models.CharField(max_length=50, verbose_name="Название")
 
     email: str = models.EmailField(max_length=100, unique=True, verbose_name="Email")
     country: str = models.CharField(max_length=50, verbose_name="Страна")
     city: str = models.CharField(max_length=50, verbose_name="Город")
     street: str = models.CharField(max_length=50, verbose_name="Улица")
-    house: str = models.CharField(max_length=10, verbose_name="Дом")
+    house_number: str = models.CharField(max_length=10, verbose_name="Дом")
+
+    supplier: int = models.ForeignKey("self", on_delete=models.SET_NULL, **NULLABLE)
+    debt: float = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Дебеторская задолженность"
+    )
+    creation_date = models.DateTimeField(
+        default=timezone.now, verbose_name="Дата создания"
+    )
+
+    def get_hierarchy_level(self) -> int:
+        level = 0
+        current_node = self.supplier
+        while current_node:
+            level += 1
+            current_node = current_node.supplier
+        return level
 
     def __str__(self) -> str:
-        return f"{self.email}, {self.country}, {self.city}, {self.street}, {self.house}"
+        return f"{self.name}, {self.email}, {self.country}, {self.city}, {self.street}, {self.house_number}"
 
     class Meta:
-        verbose_name: str = "Контакт"
-        verbose_name_plural: str = "Контакты"
+        verbose_name: str = "Сетевой узел"
+        verbose_name_plural: str = "Сетевые узлы"
 
 
 class Products(models.Model):
@@ -36,102 +53,13 @@ class Products(models.Model):
     model: str = models.CharField(max_length=50, verbose_name="Модель")
     data: str = models.DateField(verbose_name="Дата выпуска")
 
+    network_node: NetworkNode = models.ForeignKey(
+        NetworkNode, on_delete=models.CASCADE, **NULLABLE, verbose_name="Торгующая сеть"
+    )
+
     def __str__(self) -> str:
         return f"{self.name}, {self.model}, {self.data}"
 
     class Meta:
         verbose_name: str = "Продукт"
         verbose_name_plural: str = "Продукты"
-
-
-class Factory(models.Model):
-    """
-    Модель завода, это производитель, он имеет нулевой уровень иерархии среди всех поставщиков
-    по ТЗ должно быть поле задолженности, но т.к. завод имеет нулевой уровень в иерархии,
-    по умолчанию он никому не должен
-    """
-
-    name: str = models.CharField(max_length=50, verbose_name="Название")
-    contacts: Contacts = models.OneToOneField(
-        Contacts, on_delete=models.CASCADE, verbose_name="Контакты", **NULLABLE
-    )
-    products: Products = models.ManyToManyField(
-        Products, verbose_name="Продукты", **NULLABLE
-    )
-    date: str = models.DateField(
-        verbose_name="Дата создания организации", default=datetime.date.today
-    )
-
-    def __str__(self) -> str:
-        return f"{self.name}, {self.contacts}, {self.products}"
-
-    class Meta:
-        verbose_name: str = "Завод"
-        verbose_name_plural: str = "Заводы"
-
-
-class RetailNetwork(models.Model):
-    """
-    Модель розничной сети, уровень в иерархии №1, официальный диллер завода изготовителя
-    Может иметь задолженность перед заводом
-    """
-
-    name: str = models.CharField(max_length=50, verbose_name="Название")
-    contacts: Contacts = models.OneToOneField(
-        Contacts, on_delete=models.CASCADE, verbose_name="Контакты", **NULLABLE
-    )
-    products: Products = models.ManyToManyField(
-        Products, verbose_name="Продукты", **NULLABLE
-    )
-    supplier: Factory = models.ForeignKey(
-        Factory, on_delete=models.CASCADE, verbose_name="Поставщик", **NULLABLE
-    )
-    arrears: float = models.FloatField(verbose_name="Сумма задолженности", default=0)
-    date: str = models.DateField(
-        verbose_name="Дата создания организации", default=datetime.date.today
-    )
-
-    def __str__(self) -> str:
-        return f"{self.name}, {self.contacts}, {self.products}, {self.arrears}"
-
-    class Meta:
-        verbose_name: str = "Розничная сеть"
-        verbose_name_plural: str = "Розничные сети"
-
-
-class IndividualEntrepreneur(models.Model):
-    """
-    Класс индивидуального предпринимателя, уровень в иерархии может быть любым, кроме 0,
-    может иметь задолженность перед заводом и перед розничной сетью,
-    поставщики: Завод, Розничная сеть
-    """
-
-    name: str = models.CharField(max_length=50, verbose_name="Название")
-    contacts: Contacts = models.OneToOneField(
-        Contacts, on_delete=models.CASCADE, verbose_name="Контакты", **NULLABLE
-    )
-    products: Products = models.ManyToManyField(
-        Products, verbose_name="Продукты", **NULLABLE
-    )
-    supplier_content_type = models.ForeignKey(
-        ContentType,
-        on_delete=models.CASCADE,
-        verbose_name="Тип поставщика",
-        limit_choices_to={"model__in": ("Factory", "RetailNetwork")},
-        **NULLABLE,
-    )
-    supplier_object_id = models.PositiveIntegerField(
-        verbose_name="ID поставщика", **NULLABLE
-    )
-    supplier = GenericForeignKey("supplier_content_type", "supplier_object_id")
-    arrears: float = models.FloatField(verbose_name="Сумма задолженности", default=0)
-    date: str = models.DateField(
-        verbose_name="Дата создания организации", default=datetime.date.today
-    )
-
-    def __str__(self) -> str:
-        return f"{self.name}, {self.contacts}, {self.products}, {self.supplier}"
-
-    class Meta:
-        verbose_name: str = "Индивидуальный предприниматель"
-        verbose_name_plural: str = "Индивидуальные предприниматели"
